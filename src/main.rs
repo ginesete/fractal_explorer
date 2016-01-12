46ca6620eb679dfdfc0ce04a8b5ef2bca50f5d55
Gines Espada Almansa <gines.espada@gmail.com>
2016-01-12 11:42:37 +0100

    Add main.rs

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+#![allow(dead_code)]
+#![allow(unused_imports)]
+#![allow(unused_parens)]
+#![allow(unused_variables)]
+extern crate sdl2;
+extern crate fractal_generation;
+
+use fractal_generation::*;
+
+use sdl2::event::{Event};
+use sdl2::rect::{Point};
+use sdl2::pixels::{Color};
+use sdl2::mouse::{Mouse};
+use sdl2::render::{Renderer};
+
+fn main() {
+    println!("Hello");
+}
